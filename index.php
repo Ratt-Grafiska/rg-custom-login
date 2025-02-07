34016fb3f79bed0f8f7960250df3c9e584b20d77
Johan <johan@rattgrafiska.se>
2025-02-07 08:12:09 +0100

    Update index.php
    chore: version bump and enable custom login file
    
    - Bumped plugin version from 1.0.6 to 1.0.7
    - Uncommented `require_once "rg-custom-login.php"` to ensure the custom login functionality is loaded

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Rätt Grafiska Custom Login Theme
 Plugin URI: https://github.com/Ratt-Grafiska/
 Update URI: https://github.com/Ratt-Grafiska/rg-custom-login
 Description: A WordPress plugin that customizes the login page with a custom logo, theme colors, and styling.
-Version: 1.0.6
+Version: 1.0.7
 Author: Johan Wistbacka
 Author URI: https://wistbacka.se
 License: GPL2
@@ -15,4 +15,4 @@ if (!class_exists("RgGitUpdater")) {
   require_once plugin_dir_path(__FILE__) . "rg-git-updater.php";
 }
 
-//require_once plugin_dir_path(__FILE__) . "rg-custom-login.php";
+require_once plugin_dir_path(__FILE__) . "rg-custom-login.php";
